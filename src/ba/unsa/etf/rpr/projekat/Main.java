1006776876a72a8d1d2ccb37801c651cc3eccf93
asiljak1 <asiljak1@etf.unsa.ba>
2020-08-15 19:49:29 +0200

    napravljen izgled login-a

--- src/ba/unsa/etf/rpr/projekat/Main.java
+++ src/ba/unsa/etf/rpr/projekat/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package ba.unsa.etf.rpr.projekat;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,10 +10,11 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+        primaryStage.setResizable(false);
     }
 
 
